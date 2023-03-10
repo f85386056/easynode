e815da7454c5f2f8e8d928161d50a10bb657ba30
f85386056 <61175596+f85386056@users.noreply.github.com>
2023-03-10 09:28:16 +0800

    Update easynode-server-install.sh

--- easynode-server-install.sh
+++ easynode-server-install.sh
@@ -11,7 +11,7 @@ echo '开始安装nvm'
 rm -rf /root/.nvm
 
 # 国内
-bash -c "$(curl -fsSL https://gitee.com/chaoszhu_0/nvm-cn/raw/master/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/f85386056/nvm-cn/master/install.sh)"
 # 国外
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/chaos-zhu/nvm-cn/master/install.sh)"
 
